978e75f225c5aafb3a36c1c4db56a2fb9b39ce6a
Dan Looten <danwlooten@gmail.com>
2022-04-24 13:00:31 -0500

    Format App.js jsx code to map over array

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
 const App = () => {
+
+const categories = [
+  {
+    id: 1,
+    title: "Hats",
+    subtitle: "Shop Now"
+  },
+  {
+    id: 2,
+    title: "Jackets",
+    subtitle: "Shop Now"
+  },
+  {
+    id: 3,
+    title: "Sneakers",
+    subtitle: "Shop Now"
+  },
+  {
+    id: 4,
+    title: "Men's",
+    subtitle: "Shop Now"
+  },
+  {
+    id: 5,
+    title: "Women's",
+    subtitle: "Shop Now"
+  }
+]
+
   return (
     <div className="categories-container">
+      {categories.map(({title, subtitle})=>(
       <div className="category-container">
         {/* <img src="" alt=""> */}
+        <div className="background-image"></div>
         <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+          <h2>{title}</h2>
+          <p>{subtitle}</p>
         </div>
       </div>
-      <div className="category-container">
-        {/* <img src="" alt=""> */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img src="" alt=""> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img src="" alt=""> */}
-        <div className="category-body-container">
-          <h2>Women's</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img src="" alt=""> */}
-        <div className="category-body-container">
-          <h2>Men's</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
