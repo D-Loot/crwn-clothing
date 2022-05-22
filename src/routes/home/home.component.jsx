cf2ad9d717bbedd7e42cac6ddb70083da41c0c71
Dan Looten <danwlooten@gmail.com>
2022-05-22 15:00:35 -0500

    Lesson 84-86 Set up Nav Bar, Shop page and Icon

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from 'react-router-dom';
+
 import Directory from '../../components/directory/directory.component';
 
 const Home = () => {
@@ -6,30 +8,40 @@ const Home = () => {
       id: 1,
       title: 'hats',
       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      subtitle: 'Shop Now'
     },
     {
       id: 2,
       title: 'jackets',
       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      subtitle: 'Shop Now'
     },
     {
       id: 3,
       title: 'sneakers',
       imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      subtitle: 'Shop Now'
     },
     {
       id: 4,
       title: 'womens',
       imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      subtitle: 'Shop Now'
     },
     {
       id: 5,
       title: 'mens',
       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      subtitle: 'Shop Now'
     },
   ];
 
-  return <Directory categories={categories} />;
+  return (
+    <div>
+      <Directory categories={categories} />
+      <Outlet />
+    </div>
+  );
 };
 
 export default Home;
