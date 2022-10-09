c928a4059bbfbd80cfa594e1eeb2926e929905ac
Dan Looten <danwlooten@gmail.com>
2022-10-09 12:05:49 -0500

    95 - Added Signin Component and Firebase Utils

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,12 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore'
 
 // https://console.firebase.google.com/u/0/project/crwn-clothing-dloot/overview
 
@@ -33,3 +39,37 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth)=>{
+  const userDocRef = doc(db,'users',userAuth.uid);
+  // console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  // console.log(userSnapshot);
+  // console.log(userSnapshot.exists());
+
+  // IF THE USER DOES NOT EXISTS
+  if(!userSnapshot.exists()){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef,{
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error){
+      console.log('error creating the user',error.message);
+    }
+  }
+  return userDocRef
+}
+
+
+
+
+
+
